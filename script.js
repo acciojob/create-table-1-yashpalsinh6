c0df2c6da8f104e5e3619febff30425ef7419a98
yashpalsinh6 <75825134+yashpalsinh6@users.noreply.github.com>
2024-05-01 10:02:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function insert_Row() {
-    //Write your code here
-  
+
+	let table = document.getElementById("sampleTable");
+	// var table = document.getElementById("sampleTable");
+	
+	 var newRow = table.insertRow(0);
+
+    // Insert cells into the new row
+    var leftCell = newRow.insertCell(0);
+    var rightCell = newRow.insertCell(1);
+
+    // Set the content of the cells
+    leftCell.innerHTML = "New Cell1";
+    rightCell.innerHTML = "New Cell2";
   
 }
